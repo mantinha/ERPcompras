10905ceb3a1cea552c971087e788f2fa90f72f40
adriano <adriano@192.168.15.2>
2020-07-19 01:29:58 -0300

    Primeiro Commit

---
+++ ERPcompras/src/main/java/br/fsa/compras/ErPcomprasApplication.java
@@ -0,0 +1,13 @@
+package br.fsa.compras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErPcomprasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErPcomprasApplication.class, args);
+	}
+
+}
