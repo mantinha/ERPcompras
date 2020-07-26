7864e9d10e544ee65586c1af05abdcc84129fcd2
adriano <adriano@192.168.15.2>
2020-07-26 04:35:09 -0300

    Fragments - Structure - Validating Created

--- ERPcompras/src/main/java/br/fsa/compras/controller/GerarRequisicaoDecoController.java
+++ ERPcompras/src/main/java/br/fsa/compras/controller/GerarRequisicaoDecoController.java
@@ -3,10 +3,9 @@ package br.fsa.compras.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -16,66 +15,97 @@ import br.fsa.compras.command.RequisicaoDecoCommand;
 import br.fsa.compras.exception.NotFoundException;
 import br.fsa.compras.service.RequisicaoDecoService;
 
+import javax.validation.Valid;
 @Controller
+@RequestMapping("/requisicoes/enviar")
 public class GerarRequisicaoDecoController {
 	
-	private static final String RECIPE_RECIPEFORM_URL = "gerarrequisicoes/gerarrequisicoesform";
-	/*
-	 * INJEÇÃO DE DEPENDÊNCIA
-	 */
+	private static final String REQUISICAO_REQUISICAOFORM_URL = "requisicoes/enviar/form";
+	
 	private final RequisicaoDecoService requisicaoDecoService;
 	
 	public GerarRequisicaoDecoController(RequisicaoDecoService requisicaoDecoService) {
 		this.requisicaoDecoService = requisicaoDecoService;
 	}
 
-	@GetMapping("/gerarrequisicoes/{id}/show")
-    public String showById(@PathVariable String id, Model model){
+	@RequestMapping({"","/","/form"})
+	public String form(Model model) {
+		
+		model.addAttribute("requisicao", requisicaoDecoService.getRequisicaoDeco());
 
-        model.addAttribute("recipe", requisicaoDecoService.findById(Long.valueOf(id)));
-
-        return "gerarrequisicoes/show";
-    }
+		return "/requisicoes/enviar/form";
+	}
 	
-	@GetMapping("gerarrequisicoes/new")
-    public String newGerarRequisicao(Model model){
-        model.addAttribute("requisicao", new RequisicaoDecoCommand());
+	@PostMapping({"","/","/form"})
+	public String submit(@Valid @ModelAttribute("requisicao") RequisicaoDecoCommand command, BindingResult bindingResult) {
 
-        return "gerarrequisicoes/gerarrequisicoesform";
-    }
+		if(bindingResult.hasErrors()) {
+			bindingResult.getAllErrors().forEach(objectError -> {
+				System.out.println(objectError.toString());
+			});
+			
+			return REQUISICAO_REQUISICAOFORM_URL;
+		}
+		
+		requisicaoDecoService.saveRequisicaoDecoCommand(command);
+		
+		return "redirect:/requisicoes";
+	}
+	/*
+	 * CONSULTAR REQUISIÇÃO POR ID
+	 */
+//	@GetMapping("/gerarrequisicoes/{id}/show")
+//    public String showById(@PathVariable String id, Model model){
+//
+//        model.addAttribute("recipe", requisicaoDecoService.findById(Long.valueOf(id)));
+//
+//        return "gerarrequisicoes/show";
+//    }
 	
-	@GetMapping("recipe/{id}/update")
-    public String updateRequisicao(@PathVariable String id, Model model){
-        model.addAttribute("recipe", requisicaoDecoService.findCommandById(Long.valueOf(id)));
-        return RECIPE_RECIPEFORM_URL;
-    }
+//	@GetMapping("/new")
+//    public String newGerarRequisicao(Model model){
+//        model.addAttribute("requisicao", new RequisicaoDecoCommand());
+//
+//        return "gerarrequisicoes/gerarrequisicoesform";
+//    }
+	/*
+	 * ALTERAR REQUISIÇÃO POR ID
+	 */
+//	@GetMapping("gerarrequisicoes/{id}/update")
+//    public String updateRequisicao(@PathVariable String id, Model model){
+//        model.addAttribute("recipe", requisicaoDecoService.findCommandById(Long.valueOf(id)));
+//        return RECIPE_RECIPEFORM_URL;
+//    }
 	/*
 	 * NECESSARIO
 	 */
-	@RequestMapping({"/gerarrequisicoes","/gerarrequisicoes/","/gerarrequisicoes/index"})
-	public String getRequisicaoDecoPage(Model model) {
-
-		model.addAttribute("requisicoes", requisicaoDecoService.getRequisicaoDeco());
-		
-		return "gerarrequisicoes/index";
-	}
+//	@RequestMapping({"/","/index"})
+//	public String getRequisicaoDecoPage(Model model) {
+//
+//		model.addAttribute("requisicoes", requisicaoDecoService.getRequisicaoDeco());
+//		
+//		return "gerarrequisicoes/index";
+//	}
 	
-	@PostMapping({"/gerarrequisicoes","/gerarrequisicoes/","/gerarrequisicoes/index"})
-	public String saveOrUpdate(@ModelAttribute RequisicaoDecoCommand command) {
-
-		RequisicaoDecoCommand savedCommand = requisicaoDecoService.saveRequisicaoDecoCommand(command);
-		
-		return "redirect:/gerarrequisicoes/" + savedCommand.getId() + "/show";
-	}
+//	@PostMapping({"/gerarrequisicoes","/gerarrequisicoes/","/gerarrequisicoes/index"})
+//	public String saveOrUpdate(@ModelAttribute RequisicaoDecoCommand command) {
+//
+//		RequisicaoDecoCommand savedCommand = requisicaoDecoService.saveRequisicaoDecoCommand(command);
+//		
+//		return "redirect:/gerarrequisicoes/" + savedCommand.getId() + "/show";
+//	}
 	/***********************************************************************************/
-	@GetMapping("gerarrequisicoes/{id}/delete")
-    public String deleteById(@PathVariable String id){
-
-        System.out.println("Deleting id: " + id);
-
-        requisicaoDecoService.deleteById(Long.valueOf(id));
-        return "redirect:/";
-    }
+	/*
+	 * EXCLUIR REQUISIÇÃO POR ID
+	 */
+//	@GetMapping("gerarrequisicoes/{id}/delete")
+//    public String deleteById(@PathVariable String id){
+//
+//        System.out.println("Deleting id: " + id);
+//
+//        requisicaoDecoService.deleteById(Long.valueOf(id));
+//        return "redirect:/";
+//    }
 	
 	@ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(NotFoundException.class)
--- ERPcompras/src/main/java/br/fsa/compras/controller/RequisicaoDecoController.java
+++ ERPcompras/src/main/java/br/fsa/compras/controller/RequisicaoDecoController.java
@@ -7,16 +7,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import br.fsa.compras.service.FuncionarioService;
 import br.fsa.compras.service.RequisicaoDecoService;
 
-/*
- * MAPEAMENTO DA URL
- * REQUEST
- * BUSCA RETORNAR PÁGINA PELO NOME
- */
 @Controller
+@RequestMapping("/requisicoes")
 public class RequisicaoDecoController {
-	/*
-	 * INJEÇÃO DE DEPENDÊNCIA
-	 */
+	
 	private final RequisicaoDecoService requisicaoDecoService;
 	private final FuncionarioService funcionarioService;
 	
@@ -26,28 +20,11 @@ public class RequisicaoDecoController {
 		this.funcionarioService = funcionarioService;
 	}
 	
-	@RequestMapping({"/painelderequisicoes","/painelderequisicoes/","/painelderequisicoes/index"})
-	public String getPainel(Model model) {
-
+	@RequestMapping({"","/","/list"})
+	public String list(Model model) {
 		model.addAttribute("funcionarios", funcionarioService.getFuncionarios());
 		model.addAttribute("requisicoes", requisicaoDecoService.getRequisicaoDeco());
-		
-		return "painelderequisicoes/index";
+		return "/requisicoes/list";
 	}
 	
-//	@PostMapping({"/gerarrequisicoes","/gerarrequisicoes/","/gerarrequisicoes/index"})
-//	public String saveOrUpdate(@ModelAttribute RequisicaoDecoCommand command) {
-//
-//		RequisicaoDecoCommand savedCommand = requisicaoDecoService.saveRequisicaoDecoCommand(command);
-//		
-//		return "redirect:/gerarrequisicoes/" + savedCommand.getId() + "/show";
-//	}
-	
-//	@RequestMapping({"","/","/index"})
-//	public String showById(@PathVariable String id, Model model) {
-//		
-//		model.addAttribute("requisicao", requisicaoDeComprasService.findById(Long.valueOf(id)));
-//		
-//		return "painelderequisicoes/index";
-//	}
 }
